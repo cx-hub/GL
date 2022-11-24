b241564988d259bc6c0487c41c955d759bb01092
cx <2743852840@qq.com>
2022-11-24 15:54:22 +0800

    first commit

---
+++ 2/Code/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/Renderer.cpp" "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/Scene.cpp" "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/main.cpp" "/home/fishermanykx/文档/WeChat Files/ykx17873536851/FileStorage/File/2022-01/RayTracing_Assignment/Code/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
